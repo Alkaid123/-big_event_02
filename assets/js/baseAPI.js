726a301ff281b94a02e14c51a69b28d4c23eed96
fay <1157145187@qq.com>
2021-03-16 12:54:17 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -8,5 +8,23 @@ $(function () {
         // console.log(options);
         // console.log(options.url);
         options.url = baseURL + options.url;
+
+        // 如果 
+        if (options.url.indexOf('/my/') != -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        };
+
+        options.complete = function (res) {
+            // console.log(res.responseJSON);
+            let obj = res.responseJSON;
+            if (obj.status == 1 && obj.message == "身份认证失败！") {
+                // 清空本地token
+                localStorage.removeItem('token');
+                // 跳转到 登录页面
+                location.href = '/login.html';
+            }
+        }
     });
 });
